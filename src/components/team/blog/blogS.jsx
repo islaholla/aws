5bd5fcc78c731d2e04122f70190f4494be6069d9
islahola <islah.khofifah@gmail.com>
2024-01-31 11:29:24 +0700

    oke

--- src/components/team/blog/blogS.jsx
+++ src/components/team/blog/blogS.jsx
@@ -8,7 +8,7 @@ const BlogS = () => {
       <section className='blog-solution storage'>
         <h1>Why AWS Storage is required?</h1>
         <h2 className="container"> <br />Amazon Web Services (AWS) storage has undergone significant diversification over time to meet a wide range of applications. New data storage technologies have changed and are continuously developing daily as a result of the dramatically growing amount of data.</h2>
-        <div className='container grid4'>
+        <div className='container grid4-custom'>
           <BlogCardS />
         </div>
       </section>
