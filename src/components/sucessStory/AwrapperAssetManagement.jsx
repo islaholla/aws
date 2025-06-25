f2d37270500bac3eb7e1c11d04feb8e43b4a78e8
Andi Ubay Baisha <andi.ubay@mastersystem.co.id>
2025-06-25 15:23:49 +0700

    update

--- src/components/sucessStory/AwrapperAssetManagement.jsx
+++ src/components/sucessStory/AwrapperAssetManagement.jsx
@@ -17,7 +17,7 @@ const AwrapperAssetManagement = () => {
       <section className='homeAbout' id="sukses_aws">
         <Heading title='Managed Security Enhancement for Asset Management Company' />
         <div className=" bg-white">
-          <h4>Executive Summary</h4>
+          <h3>Executive Summary</h3>
           One Indonesian Investment Management Company is rapidly evolving, requiring
           secure, cost-effective, and highly available IT operations to support financial
           transactions and portfolio management. This case study explores how
@@ -28,8 +28,8 @@ const AwrapperAssetManagement = () => {
           efficiently to support its growing digital investment platforms.
 
           <br /> <br />
-          <h4>        Challange
-          </h4>
+          <h3>        Challange
+          </h3>
           The company requires a comprehensive security solution that provides 24/7
           security monitoring to detect and mitigate potential threats proactively. This
           includes real-time threat detection and automated incident response to minimize
@@ -45,35 +45,44 @@ const AwrapperAssetManagement = () => {
 
 
           <br /> <br />
-          <h4>Solution</h4>
-          <h6>Mastersystem MSSP Security Services</h6>
+          <h3>Solution</h3>
+          <h4>Mastersystem MSSP Security Services</h4>
           To address its security needs, the asset management company adopted
           Mastersystem MSSP with the following key implementations:
+          <br />
           • Provide continues 24/7 Incident Response Teams Monitoring of
           Mastersystem MSSP.
+          <br />
           • Implemented AWS GuardDuty, AWS Security Hub, and AWS CloudTrail to
           provide continuous security event monitoring across AWS accounts,
           identifying potential threats such as unauthorized access, malware, or
           abnormal network activity on production workload account.
+          <br />
           • Automated incident response playbooks we created using AWS Lambda
           and AWS Systems Manager, to ensuring rapid containment of security
           incidents. This will include predefined responses for financial fraud
           attempts, unauthorized API access, and insider threats.
+          <br />
           • Utilize AWS Config to maintain continuous compliance with CIS, ISO 27001
           regulations. These services will automate compliance audits, generate
           security reports, and enforce governance policies.
+          <br />
           • Implemented Role-based access controls using AWS IAM, enforcing least-
           privilege policies to restrict access to critical systems. Multi-factor
           authentication (MFA) will be required for all privileged users, and access
           logs will be monitored via AWS CloudTrail.
+          <br />
           • AWS Security Hub and AWS Systems Manager we used to monitor and
           secure Amazon EC2 instances. Workload encryption and runtime protection
           measures will be enforced to prevent unauthorized modifications.
+          <br />
           • Web Application Firewalls (WAF) are used to protect customer web
           application against DDoS attacks and other internet-based threats.
+          <br />
           • Integrated Sophos Next-Generation Firewall (NGFW) on AWS to enhance
           security for VPN clients, ensuring secure remote access and network
           protection.
+          <br />
           • We have implemented AWS Direct Connect as the primary connection and
           AWS Site-to-Site VPN as the backup connection between corporate
           datacenter and AWS Virtual Private Clouds (VPCs). This service establishes a
@@ -82,23 +91,29 @@ const AwrapperAssetManagement = () => {
           on-premises backup solutions to AWS Cloud.
 
           <br /><br />
-          <h4>Expected Benefit</h4>
+          <h3>Expected Benefit</h3>
           By leveraging AWS-native security and Managed Services Security, companies can
           expect:
+          <br />
           • Security & Risk Reduction
+          <br />
           Company gains 99% security system uptime for proactive threat detection
           and real-time mitigation capabilities. This significantly 90% reduce the risk of
           financial data breaches, fraud, and cyberattacks, ensuring the security of
           client investments. 50% increase in security team efficiency with automated
           incident response will further minimize business disruptions caused by
           security threats.
+          <br />
           • Compliance & Regulatory Readiness
+          <br />
           With AWS-native is 100% compliance with industry security standards
           compliance solutions will ensure continuous security posture monitoring,
           helping the company meet regulatory obligations without extensive manual
           intervention. Audit-ready security reports generated automatically,
           simplifying regulatory compliance and reducing audit preparation time.
+          <br />
           • Cost & Operational Efficiency
+          <br />
           Leveraging AWS-native security services will reduce up to 30-40% compare
           with third-party security tool expenses while enhancing security automation.
           This will lower operational costs associated with manual threat detection
@@ -106,7 +121,7 @@ const AwrapperAssetManagement = () => {
           initiatives rather than routine monitoring.
 
           <br /><br />
-          <h4>Conclution</h4>
+          <h3>Conclution</h3>
           By strategically using AWS and Mastersystem AWS Managed Service Security, the
           client transformed IT infrastructure, improving cost efficiency, security, and
           scalability. As Indonesia investment sector continues to grow, AWS remains a critical
--- src/components/sucessStory/AwrapperTelco.jsx
+++ src/components/sucessStory/AwrapperTelco.jsx
@@ -17,7 +17,7 @@ const AwrapperTelco = () => {
       <section className='homeAbout' id="sukses_aws">
         <Heading title='Managed Security Services for Telecommunication Company' />
         <div className=" bg-white">
-          <h4>Executive Summary</h4>
+          <h3>Executive Summary</h3>
           This case study explores how Mastersystem Infotama Managed Service Security
           Provider (MSSP) helped a leading telecommunications provider in Indonesia
           through the implementation of AWS Managed Services. Driven by the need for
@@ -27,48 +27,58 @@ const AwrapperTelco = () => {
           telecommunications landscape.
 
           <br /> <br />
-          <h4>        Challange
-          </h4>
+          <h3>        Challange
+          </h3>
           • Security monitoring and incident response in a cloud environment for
           Business Intelligence (BI) workload.
+          <br />
           • Reducing operational overhead while managing security risks.
+          <br />
           • Company also seeks to optimize security costs by leveraging AWS-native
           solutions rather than relying on expensive third-party security tools.
 
 
           <br /> <br />
-          <h4>Solution</h4>
-          <h6>Mastersystem MSSP Security Services</h6>
+          <h3>Solution</h3>
+          <h4>Mastersystem MSSP Security Services</h4>
           To address security needs, Mastersystem MSSP services adopted AWS-native
           security tools into its existing operations. These efforts encompass:
           • Continuous 24/7 by Mastersystem MSSP SOC Team.
+          <br />
           • Utilize AWS GuardDuty and AWS Security Hub to fortify the client
           cybersecurity posture and protect against evolving threats especially on
           their production workload.
+          <br />
           • Ensured regulatory compliance through the implementation of AWS
           Identity and Access Management (IAM) policies, AWS CloudTrail, and AWS
           IAM Access Analyzer for meticulous auditing and logging of activities.
+          <br />
           • Automated incident response playbooks will be created using AWS Lambda
           to ensuring rapid containment of security incidents. This will include
           predefined responses for financial fraud attempts, unauthorized API
           access, and insider threats.
+          <br />
           • We utilize AWS Config to maintain continuous compliance with CIS, ISO
           27001 regulations. These services will automate compliance audits,
           generate security reports, and enforce governance policies.
+          <br />
           • Role-based access controls will be implemented using AWS IAM, enforcing
           least-privilege policies to restrict access to critical systems. Multi-factor
           authentication (MFA) will be required for all privileged users, and access
           logs will be monitored via AWS CloudTrail.
+          <br />
           • Centralized management and analysis of security logs is done by the
           customer and other SOC partner, while the our team takes care of timely
           actions and incident management.
+          <br />
           • Implemented Amazon CloudWatch Dashboards to provide real-time
           monitoring and insights into system performance.
+          <br />
           • Implemented Amazon CloudWatch Alarms to trigger automated responses
           and notify IT teams of potential issues using Amazon SNS.
 
           <br /><br />
-          <h4>Result and Benefit</h4>
+          <h3>Result and Benefit</h3>
           By utilizing AWS-Native service and Mastersystem MSSP Security Service delivered
           measurable improvements, including:
 
@@ -76,23 +86,27 @@ const AwrapperTelco = () => {
           clock threat detection, analysis, and incident response to ensure immediate
           action on any security anomaly, reducing Mean Time to Detect (MTTD) and
           Mean Time to Respond (MTTR).
+          <br />
           • Improved Security System Uptime: Minimized system downtime through
           proactive monitoring and timely issue resolution, achieving a 99.99% service
           availability rate. This will significantly reduce the risk of financial data
           breaches, fraud, and cyberattacks, ensuring the security of client
           investments. Automated incident response will further minimize business
           disruptions caused by security threats.
+          <br />
           • Reduction Costs & Operational Efficiency: Leveraging AWS-native security
           services will reduce up to 40% compare with third-party security tool
           expenses while enhancing security automation. 50% increase in security
           team efficiency, this will lower operational costs associated with manual
           threat detection and incident response, allowing security teams to focus on
           strategic security initiatives rather than routine monitoring.
+          <br />
           • Enhanced Security: Strengthened the security posture with continuous
           vulnerability assessments, automated patch management, and robust
           access controls, ensuring compliance with industry standards and
           safeguarding critical data, resulting up to 75% improvement in security
           score.
+          <br />
           With these results, through strategic collaboration with an AWS-focused MSSP, the
           telecom provider successfully transformed its cloud operations, significantly reduced
           costs, improved security, and enhanced service delivery. The implementation of a
